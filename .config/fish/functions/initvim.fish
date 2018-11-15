19fb6a571ff43eb27ce2ea2c84f9ff24f84e3595
Phantop <phantop@tuta.io>
2018-11-14 21:06:42 -0500

    Remove initvim script after removing vim-plug

--- .config/fish/functions/initvim.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /tmp/fish.J0WeiJ/initvim.fish @ line 2
-function initvim
-	nvim ~/.config/nvim/init.vim
-        nvim +PlugInstall +PlugUpdate +PlugUpgrade +PlugClean +q +q +q +q +q
-end
