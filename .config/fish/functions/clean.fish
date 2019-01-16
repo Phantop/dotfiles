60aeefd70ca7074132b6a7d42b3b12f6b3a4e17d
Phantop <phantop@tuta.io>
2019-01-15 19:39:37 -0500

    Fish: clean - don't clean memory to reduce hangs

--- .config/fish/functions/clean.fish
+++ .config/fish/functions/clean.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.XLdbAh/clean.fish @ line 2
+# Defined in /tmp/fish.gPi3gm/clean.fish @ line 2
 function clean
 	s printf \ 
     font
@@ -7,8 +7,8 @@ function clean
     se rmo -y
     se rdb -y
     s journalctl --vacuum-size=1K
-    bleachbit -c (bleachbit -l|grep -Ev 'system.free_disk_space|transmission')
-    s bleachbit -c (bleachbit -l|grep -Ev 'system.free_disk_space|transmission')
+    bleachbit -c (bleachbit -l|grep -Ev 'system.free_disk_space|transmission|system.memory')
+    s bleachbit -c (bleachbit -l|grep -Ev 'system.free_disk_space|transmission|systen.memory')
     rm -r ~/.cache/*
     rm -r ~/.nv/*
     rm -r ~/.local/share/nvim/swap/
