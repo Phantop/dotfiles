e1acbc8ef3f8aa00d5cf7722d345d2e052efb9cf
Phantop <phantop@tuta.io>
2019-01-20 14:14:18 -0500

    Fish: Make wallo remove previous wall0

--- .config/fish/functions/wallo.fish
+++ .config/fish/functions/wallo.fish
@@ -1,4 +1,5 @@
-# Defined in /tmp/fish.Z2h5NN/wallo.fish @ line 2
+# Defined in /tmp/fish.LxG0t1/wallo.fish @ line 2
 function wallo
+	rm ~/.config/wall0
 	convert $argv -resize (xrandr | grep '*'| grep -o [0-9]\*x[0-9]\*) ~/.config/wall0
 end
