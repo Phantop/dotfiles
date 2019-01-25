a425fa63b1b40ca4cfb4cb59bdd6ff23355f322b
Phantop <phantop@tuta.io>
2019-01-24 19:38:59 -0500

    Update submodules and submodule updater

--- .config/fish/functions/gsu.fish
+++ .config/fish/functions/gsu.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.bDC1Cu/gsu.fish @ line 2
+# Defined in /tmp/fish.tGpsnp/gsu.fish @ line 2
 function gsu
-	g submodule foreach --recursive 
-        g pull origin master $argv
+	g submodule update --init --recursive
+    g submodule foreach git pull origin master
 end
