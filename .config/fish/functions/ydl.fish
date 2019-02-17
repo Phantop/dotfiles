e03f6bebc516907027c92fb3e8029f3cd34cb47d
Phantop <phantop@tuta.io>
2019-02-17 11:58:53 -0500

    Fish: ydl with aria2c

--- .config/fish/functions/ydl.fish
+++ .config/fish/functions/ydl.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.sml9kE/ydl.fish @ line 2
+# Defined in /tmp/fish.YYmq1V/ydl.fish @ line 2
 function ydl
-	youtube-dl --continue --ignore-errors --no-overwrites $argv
+	youtube-dl --continue --ignore-errors --no-overwrites --external-downloader=aria2c $argv
 end
