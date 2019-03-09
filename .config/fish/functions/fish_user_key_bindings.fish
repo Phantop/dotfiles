1b9c4f4ded057d1b6279fd9130d260f00260d136
Phantop <phantop@tuta.io>
2019-03-09 11:54:35 -0500

    Fish: Map a-g to show git log

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
-# Defined in /tmp/fish.JFpPwv/fish_user_key_bindings.fish @ line 2
+# Defined in /tmp/fish.qZPxTq/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
 	bind \co 'f'
     bind \eo 'cd (ffd | fzf)'
     bind \ed 'ncdu'
+    bind \eg 'gl'
 end
