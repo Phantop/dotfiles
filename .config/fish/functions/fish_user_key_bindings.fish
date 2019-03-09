cad72e4b32e073611c34c9cf79df218f08e02f6b
Phantop <phantop@tuta.io>
2019-03-09 18:44:17 -0500

    Fish: alt-g also show git status

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.qZPxTq/fish_user_key_bindings.fish @ line 2
+# Defined in /tmp/fish.48Jh0J/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
 	bind \co 'f'
     bind \eo 'cd (ffd | fzf)'
     bind \ed 'ncdu'
-    bind \eg 'gl'
+    bind \eg 'gl; gs'
 end
