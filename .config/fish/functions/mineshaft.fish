ea1a7d7dc695dbea3d2fb6b2cb13548ddc0f2ff5
Phantop <phantop@tuta.io>
2019-03-25 16:54:56 -0400

    Fish: Mineshafter function

---
+++ .config/fish/functions/mineshaft.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.aAXDro/mineshaft.fish @ line 1
+function mineshaft
+	cd /tmp
+    wget https://mineshafter.info/files/jars/Mineshafter-launcher.jar
+    java -jar Mineshafter-launcher.jar
+end
