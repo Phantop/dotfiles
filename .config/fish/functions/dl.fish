f2e492b105013357fa287d58dc85573f79d8dcf2
Phantop <phantop@tuta.io>
2019-03-25 19:24:01 -0400

    Fish: Make dl download when given args

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,4 +1,8 @@
-# Defined in /tmp/fish.A1YqAp/dl.fish @ line 1
+# Defined in /tmp/fish.0D6Y21/dl.fish @ line 2
 function dl
+	if test $argv
+        wget $argv
+    else
 	cd ~/Downloads/
+    end
 end
