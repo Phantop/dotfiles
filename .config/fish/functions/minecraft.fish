0cb90c50dcd2177e4e86126448529c9861ad35c2
Phantop <phantop@tuta.io>
2019-03-28 17:01:43 -0400

    Fish: Change Minecraft function a bit

--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.3jo41f/minecraft.fish @ line 2
+# Defined in /tmp/fish.HFRAAc/minecraft.fish @ line 2
 function minecraft
-	cd $D/Games/Minecraft/Game; rm (ff -e log); ./MultiMC
+	cd $D/Games/Minecraft/Game; rm (ff -e log); bin/MultiMC -d .
 end
