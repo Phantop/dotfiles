53c52815da11d81a9b1a661d53f7d13c6557a4ea
Phantop <phantop@tuta.io>
2019-03-28 16:58:56 -0400

    Fish: gzexe and upx functions

---
+++ .config/fish/functions/gzexe.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.qu78NN/gzexe.fish @ line 1
+function gzexe
+	$D/Tools/gzexe $argv
+end
---
+++ .config/fish/functions/upx.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.CvTXoj/upx.fish @ line 1
+function upx
+	$D/Tools/upx $argv
+end
