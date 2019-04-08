55a59677b13ab31cad37b2e9aa3f463a6a4e6d02
Phantop <phantop@tuta.io>
2019-04-08 16:58:54 -0400

    Fish: Clean up PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # PATHS #
 set -gx ANDROID_HOME $D/Installs/Android/sdk
-set -gx fish_user_paths $D/Tools ~/.local/bin /usr/bin /usr/local/bin /bin /usr/sbin /usr/local/sbin /sbin $PLAN9/bin $ANDROID_HOME/platform-tools
+set -gx fish_user_paths $D/Tools $ANDROID_HOME/platform-tools /sbin /bin /usr/sbin /usr/bin /usr/local/sbin /usr/local/bin
 
 # PROGRAM SETTINGS #
 set -gx EDITOR nvim
