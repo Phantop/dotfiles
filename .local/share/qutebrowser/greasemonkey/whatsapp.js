7008fdac5f5ffeb791ebf1386f40bf3a8e1f9968
Phantop <phantop@tuta.io>
2019-04-08 16:52:53 -0400

    Qutebrowser: Fix WhatsApp (fuck FaceBook)

--- .local/share/qutebrowser/greasemonkey/whatsapp.js
+++ .local/share/qutebrowser/greasemonkey/whatsapp.js
@@ -1171,3 +1171,6 @@ if (typeof GM_addStyle != "undefined") {
 	}
 }
 })();
+
+if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) === 0)
+	navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
