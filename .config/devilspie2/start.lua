dd783905ca3b7d65f3aa063d870cde0145964cbe
Phantop <phantop@tuta.io>
2019-04-20 13:45:19 -0400

    Devilspie: Remove gmusicbrowser. move Steam

--- .config/devilspie2/start.lua
+++ .config/devilspie2/start.lua
@@ -13,13 +13,8 @@ set_on_top()
 end
 
 if (get_application_name()=="Steam") then
-set_window_workspace(2)
-minimize()
-end
-
-if (get_application_name()=="gmusicbrowser") then
 set_window_workspace(3)
 maximize_vertically()
-xywh(0,0,960,1080)
+xywh(960,0,960,1080)
 set_on_top()
 end
