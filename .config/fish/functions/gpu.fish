60cb5f8283faebeccbef5b23059ca2af510234df
Phantop <phantop@tuta.io>
2019-04-20 19:31:11 -0400

    Fish: Add mpvd(isown) function

--- .config/fish/functions/gpu.fish
+++ .config/fish/functions/gpu.fish
@@ -1,3 +1,4 @@
-function gpu 
+# Defined in /home/glados/.config/fish/functions/gpu.fish @ line 1
+function gpu
 	git push $argv
 end
---
+++ .config/fish/functions/mpvd.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.jwAYHg/mpvd.fish @ line 1
+function mpvd
+	mpv $argv & disown;exit
+end
