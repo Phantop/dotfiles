979a9cc1c5bd436a02ce09a08a6914702910dc9c
Phantop <phantop@tuta.io>
2019-04-20 01:11:59 -0400

    Improve squash function and fontconfig

--- .config/fish/functions/squash.fish
+++ .config/fish/functions/squash.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.Ce3uv4/squash.fish @ line 2
 function squash
-	mksquashfs . ../(basename (pwd))Squash -comp xz -b 1M
+	mksquashfs . ../(basename (pwd))Squash -comp xz -b 1M -Xdict-size 1M
 end
