14a4bfba2aefe97ba59ce14834b104781f9c6cb7
Phantop <phantop@tuta.io>
2019-05-05 12:32:40 -0400

    Fish: add back MultiMC instance functions

--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,3 +1,11 @@
+# Defined in /tmp/fish.pQxchw/minecraft.fish @ line 2
 function minecraft
-	cd $D/Games/Minecraft/Game; rm (ff -e log); bin/MultiMC -d . $argv
+	cd $D/Games/Minecraft/Game
+        rm (ff -e log)
+        bin/MultiMC -d . $argv
+        for i in (ls -d instances/*/ | grep -v _MMC_TEMP | xargs -n1 basename)
+            echo "function $i
+            minecraft -l $i
+        end" > function/$i.fish
+        end
 end
