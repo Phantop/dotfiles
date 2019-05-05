ba7f0ff9bd34f2e1eda7efb2d141ee286bf922d3
Phantop <phantop@tuta.io>
2019-05-05 17:54:14 -0400

    Devilspie2: Remove Transmission and maximize Quod

--- .config/devilspie2/start.lua
+++ .config/devilspie2/start.lua
@@ -1,15 +1,6 @@
 if (get_application_name()=="Quod Libet") then
 set_window_workspace(3)
-maximize_vertically()
-xywh(0,0,960,1080)
-set_on_top()
-end
-
-if (get_application_name()=="Transmission") then
-set_window_workspace(3)
-maximize_vertically()
-xywh(960,0,960,1080)
-set_on_top()
+maximize()
 end
 
 if (get_application_name()=="Steam") then
