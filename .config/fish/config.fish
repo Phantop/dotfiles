780480d577b272bbded356f33617c928bd521936
Phantop <phantop@tuta.io>
2019-05-11 12:51:06 -0400

    Fish: Make Minecraft also make Qute functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # PATHS #
 set -gx ANDROID_HOME $D/Installs/Android/sdk
 set  fish_user_paths $D/Tools $ANDROID_HOME/platform-tools /sbin /bin /usr/sbin /usr/bin /usr/local/sbin /usr/local/bin
-set  fish_function_path $D/Games/Minecraft/Game/function $fish_function_path
+set  fish_function_path $D/Games/Minecraft/Game/functions $fish_function_path
 
 # PROGRAM SETTINGS #
 set -gx EDITOR nvim
--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,11 +1,15 @@
-# Defined in /tmp/fish.pQxchw/minecraft.fish @ line 2
+# Defined in /tmp/fish.8eD7b7/minecraft.fish @ line 2
 function minecraft
 	cd $D/Games/Minecraft/Game
         rm (ff -e log)
         bin/MultiMC -d . $argv
+        rm functions/*
         for i in (ls -d instances/*/ | grep -v _MMC_TEMP | xargs -n1 basename)
-            echo "function $i
-            minecraft -l $i
-        end" > function/$i.fish
+            echo  > functions/$i.fish "function $i
+            minecraft -l $i; end"
+        end
+        for i in (cat ~/.config/qutebrowser/quickmarks)
+            echo > functions/(echo $i| awk '{print $1;}').fish function (echo $i| awk '{print $1;}') -V "i
+            qb (echo \"$i\"| cut -d ' ' -f2); end"
         end
 end
