f0a5183b6729329c4e10037192debc2cae9a7c2b
Phantop <phantop@tuta.io>
2019-05-15 16:22:32 -0400

    Fish: better quickmark functions

--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.8eD7b7/minecraft.fish @ line 2
+# Defined in /tmp/fish.WOu8ZA/minecraft.fish @ line 2
 function minecraft
 	cd $D/Games/Minecraft/Game
         rm (ff -e log)
@@ -8,8 +8,8 @@ function minecraft
             echo  > functions/$i.fish "function $i
             minecraft -l $i; end"
         end
-        for i in (cat ~/.config/qutebrowser/quickmarks)
-            echo > functions/(echo $i| awk '{print $1;}').fish function (echo $i| awk '{print $1;}') -V "i
-            qb (echo \"$i\"| cut -d ' ' -f2); end"
+        for i in (cat ~/.config/qutebrowser/quickmarks | cut -d ' ' -f1)
+            echo > functions/$i.fish function $i"
+            qb / \":quickmark-load $i\"; end"
         end
 end
