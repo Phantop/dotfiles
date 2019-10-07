9207a70159d584a9824ba38a5362230965bc1932
Phantop <phantop@tuta.io>
2019-10-07 15:33:07 -0400

    Fish: more logic fruitdl syntax

--- .config/fish/functions/fruitdl.fish
+++ .config/fish/functions/fruitdl.fish
@@ -1,7 +1,7 @@
+# Defined in /tmp/fish.pU4dvl/fruitdl.fish @ line 2
 function fruitdl
-   cd $D/Games/Emulators/$argv[2] 
-   rm -r $argv[1]
-   set file (curl -sL github.com/$argv[2]-emu/$argv[2]-$argv[1]/releases/latest| grep -om1 $argv[2]-emu/$argv[2]-$argv[1]/releases/download/$argv[1]-[0-9]\*/$argv[2]-linux-[0-9]\*-[0-9a-f]\*).7z
+	rm -r $argv[2]
+   set file (curl -sL github.com/$argv[1]-emu/$argv[1]-$argv[2]/releases/latest| grep -om1 $argv[1]-emu/$argv[1]-$argv[2]/releases/download/$argv[2]-[0-9]\*/$argv[1]-linux-[0-9]\*-[0-9a-f]\*).7z
    dl http://github.com/$file
    set file (basename $file)
    7z x $file
