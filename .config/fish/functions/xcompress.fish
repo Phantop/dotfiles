683bca28485e9ef31ab485e6c36fd401b2c9833d
Phantop <phantop@tuta.io>
2020-01-13 16:34:04 -0500

    Fish: fix zip.xz

--- .config/fish/functions/xcompress.fish
+++ .config/fish/functions/xcompress.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.JK2lNx/xcompress.fish @ line 2
+# Defined in /tmp/fish.RFrpYE/xcompress.fish @ line 2
 function xcompress
 	if test -d $argv
-        tar -0r $argv.zip $argv
+        zip -0r $argv.zip $argv
         xz -efvk9T 0 $argv.zip
         rm $argv.zip
     else
