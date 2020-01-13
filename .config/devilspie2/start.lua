07f39b99231e33d56a4a3d2736ca7373ac8e52d0
Phantop <phantop@tuta.io>
2020-01-13 18:12:49 -0500

    dp2: Zenity Information on top; qb: no Amazon

--- .config/devilspie2/start.lua
+++ .config/devilspie2/start.lua
@@ -1,8 +1,12 @@
 if (get_application_name()=="Quod Libet") then
-set_window_workspace(3)
-maximize()
+    set_window_workspace(3)
+    maximize()
 end
 
 if (get_application_name()=="Steam") then
-set_window_workspace(2)
+    set_window_workspace(2)
+end
+
+if (get_window_name()=="Information") then
+    make_always_on_top()
 end
