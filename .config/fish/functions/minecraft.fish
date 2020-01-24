8bbdadc2b9dfa58239e34760532331cad491afe5
Phantop <phantop@tuta.io>
2020-01-24 15:00:56 -0500

    Fish: minecraft autoload java squashfs

--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,7 +1,10 @@
-# Defined in /tmp/fish.WOu8ZA/minecraft.fish @ line 2
+# Defined in /tmp/fish.ISD2gG/minecraft.fish @ line 2
 function minecraft
 	cd $D/Games/Minecraft/Game
         rm (ff -e log)
+        if not test -e /run/media/glados/disk/jdk/bin/java
+            gnome-disk-image-mounter $D/Installs/squash/java
+        end
         bin/MultiMC -d . $argv
         rm functions/*
         for i in (ls -d instances/*/ | grep -v _MMC_TEMP | xargs -n1 basename)
