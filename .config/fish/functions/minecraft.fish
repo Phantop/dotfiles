b715978e69254e17ea24466e513687d7d8dc0023
Phantop <phantop@tuta.io>
2020-02-23 19:38:46 -0500

    Fish: MC java mount w/ caja; Qute: more JS perms

--- .config/fish/functions/minecraft.fish
+++ .config/fish/functions/minecraft.fish
@@ -1,18 +1,22 @@
-# Defined in /tmp/fish.ISD2gG/minecraft.fish @ line 2
+# Defined in /tmp/fish.Yuay2h/minecraft.fish @ line 2
 function minecraft
-	cd $D/Games/Minecraft/Game
-        rm (ff -e log)
-        if not test -e /run/media/glados/disk/jdk/bin/java
-            gnome-disk-image-mounter $D/Installs/squash/java
+    cd $D/Games/Minecraft/Game
+    rm (ff -e log)
+    if not test -e /run/media/glados/disk/jdk/bin/java
+        gnome-disk-image-mounter $D/Installs/squash/java
+        if not pgrep caja
+            caja & sleep 1
+            kj
         end
-        bin/MultiMC -d . $argv
-        rm functions/*
-        for i in (ls -d instances/*/ | grep -v _MMC_TEMP | xargs -n1 basename)
-            echo  > functions/$i.fish "function $i
-            minecraft -l $i; end"
+    end
+    bin/MultiMC -d . $argv
+    rm functions/*
+    for i in (ls -d instances/*/ | grep -v _MMC_TEMP | xargs -n1 basename)
+        echo  > functions/$i.fish "function $i
+        minecraft -l $i; end"
         end
         for i in (cat ~/.config/qutebrowser/quickmarks | cut -d ' ' -f1)
             echo > functions/$i.fish function $i"
             qb / \":quickmark-load $i\"; end"
-        end
+            end
 end
