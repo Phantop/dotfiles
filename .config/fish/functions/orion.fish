ae1db0e87541c4c06dad5de4061226fd76b42d99
Phantop <phantop@tuta.io>
2020-03-02 22:46:30 -0500

    Fish: remove orion

--- .config/fish/functions/orion.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /tmp/fish.tB7Z06/orion.fish @ line 2
-function orion
-	set -x QT_QUICK_CONTROLS_MATERIAL_BACKGROUND "#222D32"
-    set -x QT_QUICK_CONTROLS_MATERIAL_ACCENT "#84BEEA"
-    command orion
-end
