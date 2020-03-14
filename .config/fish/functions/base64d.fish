b61d9c2850c605e4900b01056852c20cb923c1b8
Phantop <phantop@tuta.io>
2020-03-14 13:07:21 -0400

    Fish: base64d remove newline

--- .config/fish/functions/base64d.fish
+++ .config/fish/functions/base64d.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.JliNyj/base64d.fish @ line 2
+# Defined in /tmp/fish.KfrP39/base64d.fish @ line 2
 function base64d
-	echo $argv | base64 -d 2>/dev/null
+	echo (echo $argv | base64 -d 2>/dev/null)
 end
