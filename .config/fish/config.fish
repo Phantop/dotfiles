afe4c30f1ca3113a2e50b4942dc857e0cffdf5f7
Phantop <phantop@tuta.io>
2020-04-26 18:44:34 -0400

    Fish: better nvimpager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set  fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
 
 # PROGRAM SETTINGS #
 set -gx EDITOR nvim
-set -gx PAGER "nvim -c 'set ft=man' -"
+set -gx PAGER nvimpager
 set -U Z_CMD "j"
 
 set -x QT_QPA_PLATFORMTHEME gtk2
