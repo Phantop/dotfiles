63bd92d2182b9b03a2bfe3107671f654a81f6673
Phantop <phantop@tuta.io>
2020-04-26 21:21:12 -0400

    Fish: optiflac with flacc too

--- .config/fish/functions/optiflac.fish
+++ .config/fish/functions/optiflac.fish
@@ -1,11 +1,21 @@
-# Defined in /tmp/fish.YGBcMf/optiflac.fish @ line 2
+# Defined in /tmp/fish.NtOqet/optiflac.fish @ line 2
 function optiflac
     mkdir out
     for i in *.flac
         ffmpeg -i $i -c:v copy -c:a flac -compression_level 12 -af aformat=s16:44100 out/$i
     end
     cd out
-    for i in (ff -e flac)
+    for i in *
+        if test (du $i | cut -f1) -lt (du ../$i | cut -f1)
+            echo $i
+            mv $i ../$i
+        end
+    end
+    cd ..
+    cp *.flac out
+    cd out
+    flacc *
+    for i in *
         if test (du $i | cut -f1) -lt (du ../$i | cut -f1)
             echo $i
             mv $i ../$i
