01a0e0c1a687e88b0f9f8160e964bd405ea3f9aa
Phantop <phantop@tuta.io>
2020-05-19 18:59:15 -0400

    Fish: build refresh sudo

--- .config/fish/functions/build.fish
+++ .config/fish/functions/build.fish
@@ -1,12 +1,15 @@
+# Defined in /tmp/fish.7e0fck/build.fish @ line 2
 function build
-	s printf \ 
-   gp
-   ./autogen.sh
-   ./configure --prefix=/usr
-   cmake -DCMAKE_INSTALL_PREFIX=/usr .
-   make
-   s make install
-   meson --prefix=/usr build
-   ninja -C build
-   sudo ninja -C build install
+    s printf ''
+    gp
+    ./autogen.sh
+    ./configure --prefix=/usr
+    cmake -DCMAKE_INSTALL_PREFIX=/usr .
+    s printf ''
+    make
+    s make install
+    meson --prefix=/usr build
+    s printf ''
+    ninja -C build
+    sudo ninja -C build install
 end
