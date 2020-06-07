9efb6015123f396495f3f60fdaa28ab3d1cc2f52
Phantop <phantop@tuta.io>
2020-06-07 11:29:24 -0400

    Fish: make podsync clean (finally)

--- .config/fish/functions/podsync.fish
+++ .config/fish/functions/podsync.fish
@@ -1,4 +1,6 @@
-# Defined in /tmp/fish.Y6YBXL/podsync.fish @ line 2
+# Defined in /tmp/fish.Og3Yo4/podsync.fish @ line 2
 function podsync
+    cd $D/Videos/YouTube/
+    ./clean
     tmux new -s 1 -d "command podsync -c $D/Videos/YouTube/config.toml"
 end
