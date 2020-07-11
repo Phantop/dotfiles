d53a2d5f5d9563c9bd3a56affc046d8e59cc8d21
Phantop <phantop@tuta.io>
2020-07-11 01:41:32 -0400

    Fish: parallel optiflac

--- .config/fish/functions/optiflac.fish
+++ .config/fish/functions/optiflac.fish
@@ -1,9 +1,7 @@
-# Defined in /tmp/fish.yFtyWn/optiflac.fish @ line 2
+# Defined in /tmp/fish.Yy0Eg4/optiflac.fish @ line 2
 function optiflac
     mkdir out
-    for i in *.flac
-        ffmpeg -i $i -c:v copy -c:a flac -compression_level 12 -af aresample=resampler=soxr:precision=32:dither_method=triangular -sample_fmt s16 $argv out/$i
-    end
+    ls *.flac | parallel ffmpeg-static -i {} -c:v copy -c:a flac -compression_level 12 -af aresample=resampler=soxr:precision=32:dither_method=triangular -sample_fmt s16 $argv out/{}
     cd out
     for i in *
         if test (du $i | cut -f1) -lt (du ../$i | cut -f1)
@@ -14,7 +12,7 @@ function optiflac
     cd ..
     cp *.flac out
     cd out
-    flacc *
+    ls | parallel flacc {}
     for i in *
         if test (du $i | cut -f1) -lt (du ../$i | cut -f1)
             echo $i
