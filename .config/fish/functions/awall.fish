89bba361f079fd0b39d28088c12d78b3f9f27a49
Phantop <phantop@tuta.io>
2020-07-13 14:13:24 -0400

    Fish: awall sleep 5 before panel

--- .config/fish/functions/awall.fish
+++ .config/fish/functions/awall.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.d5qSF9/awall.fish @ line 2
+# Defined in /tmp/fish.JkZKD9/awall.fish @ line 2
 function awall
     if test $argv
         rm ~/Videos/.wallp
@@ -6,6 +6,7 @@ function awall
     end
     kill (pgrep -f "mpv.*wallpaper")
     mpv --profile=wallpaper ~/Videos/.wallp & disown
+    sleep 5
     budgie-panel --replace & disown
     if test $argv
         ffmpeg -i ~/Videos/Walls/$argv* -vframes 1 -y ~/Videos/Walls/wall.png
