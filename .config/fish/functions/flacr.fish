971e08c2a70402b5322050c43cdab8b1c9326dd3
Phantop <phantop@tuta.io>
2020-07-15 20:43:20 -0400

    Fish: better optiflac and split to flacr

--- .config/fish/functions/flacr.fish
+++ .config/fish/functions/flacr.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.B19DWM/optiflac.fish @ line 2
-function optiflac
+# Defined in /tmp/fish.3hjAN7/flacr.fish @ line 2
+function flacr
     mkdir out
-    ls *.flac | parallel ffmpeg -i {} -c:v copy -c:a flac -compression_level 12 $argv out/{}
+    ls *.flac | parallel ffmpeg-static -i {} -c:v copy -c:a flac -compression_level 12 -af aresample=resampler=soxr:precision=32:dither_method=triangular -sample_fmt s16 $argv out/{}
     cd out
     for i in *
         if test (du $i | cut -f1) -lt (du ../$i | cut -f1)
