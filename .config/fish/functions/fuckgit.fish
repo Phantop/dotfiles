dec4f17113e21c8f079251ac0068c002bdc5f992
Phantop <phantop@tuta.io>
2020-07-20 14:50:37 -0400

    Fish: update fuckgit

--- .config/fish/functions/fuckgit.fish
+++ .config/fish/functions/fuckgit.fish
@@ -1,4 +1,6 @@
+# Defined in /tmp/fish.UMey6t/fuckgit.fish @ line 2
 function fuckgit
-	git reset --hard
-	git clean -fdx
+    git reset --hard
+    git clean -fdx
+    git gc --aggressive --prune=now
 end
