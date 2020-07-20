5df7561a77fa7c41ad6e3b001ae4b9633ecfa026
Phantop <phantop@tuta.io>
2020-07-20 16:16:58 -0400

    Fish: who even uses nix lmao

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # PATHS #
-set  fish_user_paths /usr/lib/ccache/bin $D/Installs/bin /sbin /bin /usr/sbin /usr/bin
-set  fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
+set fish_user_paths /usr/lib/ccache/bin $D/Installs/bin /sbin /bin /usr/sbin /usr/bin
+set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
 
 # PROGRAM SETTINGS #
 set -gx EDITOR nvim
@@ -8,5 +8,3 @@ set -gx PAGER nvimpager
 set -U Z_CMD "j"
 
 set -x QT_QPA_PLATFORMTHEME gtk2
-
-bax . ~/.nix-profile/etc/profile.d/nix.sh 2> /dev/null
