ab928af577dcbb4baa2a55eb2cd764e4a787d5b6
Phantop <phantop@tuta.io>
2020-07-21 11:33:16 -0400

    Fish: make getfisher actually work

--- .config/fish/functions/getfisher.fish
+++ .config/fish/functions/getfisher.fish
@@ -1,5 +1,6 @@
-# Defined in /tmp/fish.n3BGCP/getfisher.fish @ line 1
+# Defined in /tmp/fish.W9SYg1/getfisher.fish @ line 2
 function getfisher
     curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-    fisher
+    fish -c fisher
+    exec fish
 end
