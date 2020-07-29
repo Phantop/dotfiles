caaa8decc6fb8aa73f2c3fadc61a7e9f0e7b8944
Phantop <phantop@tuta.io>
2020-07-29 14:21:20 -0400

    Fish: get fisher from a submodule

--- .config/fish/functions/getfisher.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /tmp/fish.W9SYg1/getfisher.fish @ line 2
-function getfisher
-    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-    fish -c fisher
-    exec fish
-end
