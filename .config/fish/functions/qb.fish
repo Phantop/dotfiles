36b9b8000ed9ff317d24c067a07178ebf1f79bda
Phantop <phantop@tuta.io>
2020-08-03 22:41:35 -0400

    Fish: qb remove VideoDecodeStats

--- .config/fish/functions/qb.fish
+++ .config/fish/functions/qb.fish
@@ -1,3 +1,5 @@
-function qb -w qutebrowser
+# Defined in /tmp/fish.ijOPnW/qb.fish @ line 2
+function qb --wraps=qutebrowser
 	qutebrowser $argv --target auto & disown
+        rm -r VideoDecodeStats
 end
