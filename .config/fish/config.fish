055d879c25e09a42d80a8706ab6ab7e5504cfb85
Phantop <phantop@tuta.io>
2020-08-14 15:17:53 -0400

    Fish: move variables out of config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 # PATHS #
-set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin /usr/bin /usr/sbin /sbin /bin 
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
 
 # FOR INTERACTIVE TERMINAL #
--- .config/fish/functions/ff.fish
+++ .config/fish/functions/ff.fish
@@ -1,5 +1,4 @@
-function ff -w fd
-	fd\
-       -E sys -E caches -E cache -E .cache -E graphicPacks -E proc -E dosdevices -E .git\
-       -H -d 5 -L -i $argv
+# Defined in /tmp/fish.jRV9Y0/ff.fish @ line 1
+function ff --wraps=fd
+	fd -E sys -E caches -E cache -E .cache -E graphicPacks -E proc -E dosdevices -E .git -H -d 5 -L -i $argv
 end
