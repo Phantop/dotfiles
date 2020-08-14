3189943dec20bef155614bd43f5704dd48834a20
Phantop <phantop@tuta.io>
2020-08-14 11:34:09 -0400

    Fish: clean up config and show prompt only in tty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,5 @@
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin /usr/bin /usr/sbin /sbin /bin 
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
 
-# PROGRAM SETTINGS #
-set -gx EDITOR nvim
-set -gx PAGER nvimpager
-set -U Z_CMD "j"
-
-set -x QT_QPA_PLATFORMTHEME gtk2
-
-starship init fish | source
+# FOR INTERACTIVE TERMINAL #
+tty -s && starship init fish | source
--- .config/fish/functions/fish_greeting.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.hGIIv7/fish_greeting.fish @ line 2
-function fish_greeting
-
-end
