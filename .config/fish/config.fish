83f05e1dd1dffed48c7e2a4e690a442fb67e7d6c
Phantop <phantop@tuta.io>
2020-08-21 19:24:37 -0400

    mpv: add Anime4k shaders

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
+set Z_CMD "j"
+
 test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD
+
 tty -s && starship init fish | source
