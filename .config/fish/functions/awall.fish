f4e394319090af03ea9a958d31a6e21b462f4b4d
Phantop <phantop@tuta.io>
2020-08-20 23:07:31 -0400

    Fish: move awall to ~/Pictures

--- .config/fish/functions/awall.fish
+++ .config/fish/functions/awall.fish
@@ -1,14 +1,15 @@
+# Defined in /tmp/fish.XPtG1H/awall.fish @ line 2
 function awall
     if test $argv
-        rm ~/Videos/.wallp
-        ln -s ~/Videos/Walls/$argv* ~/Videos/.wallp
+        rm ~/Pictures/.wallp
+        ln -s ~/Pictures/Awall/$argv* ~/Pictures/.wallp
     end
     kill (pgrep -f "mpv.*wallpaper")
-    mpv --profile=wallpaper ~/Videos/.wallp & disown
+    mpv --profile=wallpaper ~/Pictures/.wallp & disown
     sleep 5
     budgie-panel --replace & disown
     if test $argv
-        ffmpeg -i ~/Videos/Walls/$argv* -vframes 1 -y ~/Videos/Walls/wall.png
-        wallp ~/Videos/Walls/wall.png
+        ffmpeg -i ~/Pictures/Awall/$argv* -vframes 1 -y ~/Pictures/Awall/wall.png
+        wallp ~/Pictures/Awall/wall.png
     end
 end
