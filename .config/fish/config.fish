2d355ca775659644028ba551f7b516204e3e71a3
Phantop <phantop@tuta.io>
2020-08-22 19:01:04 -0400

    Fish: set D correctly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
+test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD
+
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
-
-test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD
 
 tty -s && starship init fish | source; source /usr/share/autojump/autojump.fish
