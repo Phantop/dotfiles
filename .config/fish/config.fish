04d54d9c5445afc4d523aac084b4a14898feb6a4
Phantop <phantop@tuta.io>
2020-08-22 18:29:38 -0400

    Fish: remove fisher (and add autojump)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
-set Z_CMD "j"
 
 test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD
 
-tty -s && starship init fish | source
+tty -s && starship init fish | source; source /usr/share/autojump/autojump.fish
--- .config/fish/functions/fisher.fish
+++
@@ -1,1 +0,0 @@
-../fisher/fisher.fish
