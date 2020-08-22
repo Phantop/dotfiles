214ec62d23705ea3fbbbef8cc36dd66023e018a5
Phantop <phantop@tuta.io>
2020-08-22 15:01:08 -0400

    Fish: clean cd, dl, xsquash

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,7 +1,3 @@
-function dl -w aria2c
-	if test $argv
-       aria2c --file-allocation=none -c -x 16 -s 16 $argv
-   else
-	cd ~/Downloads/
-   end
+function dl --wraps=aria2c
+	test $argv && aria2c --file-allocation=none -c -x 16 -s 16 $argv || cd ~/Downloads/
 end
--- .config/fish/functions/xsquash.fish
+++
@@ -1,3 +0,0 @@
-function xsquash -w mksquashfs
-	mksquashfs . ../(basename (pwd))Squash -comp xz -b 1M -Xdict-size 1M $argv
-end
