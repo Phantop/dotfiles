b26ff7ec974737b286db20f60a841981509446fa
Phantop <phantop@tuta.io>
2020-08-23 12:05:38 -0400

    Fish: nicer squash function

--- .config/fish/functions/squash.fish
+++ .config/fish/functions/squash.fish
@@ -1,3 +1,4 @@
-function squash -w mksquashfs
-	mksquashfs . ../(basename (pwd))Squash -comp zstd -b 1M -Xcompression-level 22 $argv
+# Defined in /tmp/fish.N4oxkp/squash.fish @ line 2
+function squash --wraps=mksquashfs
+	mksquashfs . ../(basename $PWD).squashfs -comp zstd -b 1M -Xcompression-level 22 $argv
 end
