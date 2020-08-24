0f1f53ad9bbdfff78d0d891b8cedc57b5ea3106c
Phantop <phantop@tuta.io>
2020-08-23 22:47:09 -0400

    Fish: set $D to ~ if not aperture

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD
+test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD || set -gx D ~
 
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
