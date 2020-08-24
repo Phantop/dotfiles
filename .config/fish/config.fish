8cce502487be464af315034bacfa8329672de963
Phantop <phantop@tuta.io>
2020-08-24 18:14:01 -0400

    Setup: get solbuild; Fish: autojump only in tty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,4 @@ test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD || set -gx D ~
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
 
-tty -s && starship init fish | source; source /usr/share/autojump/autojump.fish
+tty -s && starship init fish | source && source /usr/share/autojump/autojump.fish
