4d8e4f51d30aaa391927552eb3cc1f8ba5874509
Phantop <phantop@tuta.io>
2020-08-28 16:59:33 -0400

    Fish: export pager and editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD || set -gx D ~
 
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
 set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
-set EDITOR nvim; set PAGER most
+export EDITOR=nvim PAGER=most
 
 tty -s && starship init fish | source && source /usr/share/autojump/autojump.fish
