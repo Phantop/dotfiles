2c5eb640fe2dc94ea21337ae9f3c74080b10373d
Phantop <phantop@tuta.io>
2020-09-04 23:08:30 -0400

    Fish: ydl actually get subs; setup: get git-extras

--- .config/fish/functions/ydl.fish
+++ .config/fish/functions/ydl.fish
@@ -1,3 +1,4 @@
-function ydl -w youtube-dl
-	youtube-dl --continue --ignore-errors --no-overwrites --write-sub --embed-subs -o '%(title)s.%(ext)s' $argv
+# Defined in /tmp/fish.J0dXw4/ydl.fish @ line 2
+function ydl --wraps=youtube-dl
+	youtube-dl --continue --ignore-errors --no-overwrites --write-auto-sub --write-sub --embed-subs --sub-lang=en -o '%(title)s.%(ext)s' $argv
 end
