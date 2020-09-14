d4e1e234692a2d281c6f2ee6897f3989b515495a
Phantop <phantop@tuta.io>
2020-09-14 11:35:51 -0400

    Fish: add acmus to bin, fix wallp

--- .config/fish/functions/wallp.fish
+++ .config/fish/functions/wallp.fish
@@ -1,3 +1,4 @@
-function wallp -w readlink
-    gsettings set org.gnome.desktop.background picture-uri "file://(readlink -f $argv)"
+# Defined in /tmp/fish.UKii9e/wallp.fish @ line 2
+function wallp --wraps=readlink
+    gsettings set org.gnome.desktop.background picture-uri file://(readlink -f $argv)
 end
