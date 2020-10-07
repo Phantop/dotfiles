bce30f266107b21ab6c163cf1c60025b3d9db28d
Phantop <phantop@tuta.io>
2020-10-07 08:45:22 -0400

    Minecraft: don't use Game subdir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 test (hostname) = "aperture" && set -gx D /mnt/LocalDiskD || set -gx D ~
 
 set fish_user_paths ~/.local/bin $D/Installs/bin /usr/lib/ccache/bin
-set fish_function_path $fish_function_path $D/Games/Minecraft/Game/functions
+set fish_function_path $fish_function_path $D/Games/Minecraft/functions
 export EDITOR=nvim PAGER=most
 
 tty -s && starship init fish | source && source /usr/share/autojump/autojump.fish
