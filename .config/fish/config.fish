53ee953c2012469209a65f879c372d624b1fb17b
Phantop <phantop@tuta.io>
2021-03-06 18:40:15 -0500

    Fish: update to version 3.2.0; foldwarf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set -x EDITOR nvim
 set -x PAGER most
 set -x MANPAGER "nvim -c 'set ft=man' -"
+
+fish_add_path -g ~/.local/bin
 starship init fish | source
 source /usr/share/autojump/autojump.fish
