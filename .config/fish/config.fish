8989c31d7a73320284d47a4014c02f1ad1df17a6
Phantop <phantop@tuta.io>
2021-07-25 00:19:15 -0400

    Better compress, clean, gitconfig; ccache

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ set -x EDITOR nvim
 set -x PAGER most
 set -x MANPAGER "nvim -c 'set ft=man' -"
 
-fish_add_path -g ~/.local/bin
+fish_add_path -g ~/.local/bin /usr/lib64/ccache/bin
 starship init fish | source
 source /usr/share/autojump/autojump.fish
