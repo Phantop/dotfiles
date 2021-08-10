5ba2e4fbf344c005bd2b021571b30cfd92b3bb89
Phantop <phantop@tuta.io>
2021-08-09 20:46:02 -0400

    Fish: $D on aperture and load starship differently

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ set -x EDITOR nvim
 set -x PAGER most
 set -x MANPAGER "nvim -c 'set ft=man' -"
 
+test (hostname) = aperture && set D /mnt/LocalDiskD/
+
 fish_add_path -g ~/.local/bin /usr/lib64/ccache/bin
-starship init fish | source
+starship init fish --print-full-init | source
 source /usr/share/autojump/autojump.fish
