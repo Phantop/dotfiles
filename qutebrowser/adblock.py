ea60793808eb403034c196de2b6bd949cba00193
Phantop <phantop@tuta.io>
2022-10-17 22:32:55 -0400

    qute: some theming and blocking updates

--- qutebrowser/adblock.py
+++ qutebrowser/adblock.py
@@ -1,6 +1,7 @@
 host = c.content.blocking.hosts.lists.append
 host("https://www.github.developerdan.com/hosts/lists/facebook-extended.txt")
 
+c.content.blocking.adblock.lists = []
 abp = c.content.blocking.adblock.lists.append
 abp("https://fanboy.co.nz/r/fanboy-ultimate.txt")
 abp("https://fanboy.co.nz/fanboy-antifacebook.txt")
@@ -8,6 +9,8 @@ abp("https://fanboy.co.nz/fanboy-annoyance.txt")
 abp("https://fanboy.co.nz/fanboy-cookiemonster.txt")
 abp("https://easylist-downloads.adblockplus.org/antiadblockfilters.txt")
 abp("https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt")
+abp("https://easylist-downloads.adblockplus.org/easylist.txt")
+abp("https://easylist-downloads.adblockplus.org/easyprivacy.txt")
 
 abp("https://github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt")
 abp("https://github.com/DandelionSprout/adfilt/raw/master/AnnoyancesList")
