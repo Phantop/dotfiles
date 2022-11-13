e000ba96a0130eb8395b71d4f6226fc92659383f
Phantop <phantop@tuta.io>
2022-11-13 15:22:41 -0500

    fish: update setup, source, hitolo organization

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set fish_color_command --bold
 set fish_color_comment red
 set fish_color_end brmagenta
 set fish_color_error brred
-set fish_color_escape 'bryellow'  '--bold'
+set fish_color_escape 'bryellow' '--bold'
 set fish_color_match --background=brblue
 set fish_color_operator bryellow
 set fish_color_param cyan
@@ -15,11 +15,10 @@ set fish_color_redirection brblue
 set fish_color_status red
 set fish_color_user brgreen
 set fish_color_valid_path --underline
-set fish_pager_color_prefix 'white'  '--bold'  '--underline'
+set fish_pager_color_prefix 'white' '--bold' '--underline'
 
 bind \ed 'ncdu'
-bind \ef 'fzf | clip'
+bind \ef '_fzf_wrapper | clip'
 bind \e\co 'open (_fzf_wrapper)'
 
-source /usr/share/autojump/autojump.fish
-tty -s && starship init fish --print-full-init | source && echo -en "\e[5 q"
+printf "\e[5 q"
