8dbbaf59853a8b874b180aa52e3757dc4375e863
Phantop <phantop@tuta.io>
2023-03-15 20:02:36 -0400

    qute: farside librex; bin: minecraft fabric

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,10 +6,10 @@ c.confirm_quit = ['downloads']
 c.downloads.location.directory = '~/Downloads'
 c.qt.workarounds.remove_service_workers = True
 
-c.url.searchengines['l'] = 'lx.vern.cc/search.php?q={}'
-c.url.searchengines['m'] = 'morty.ononoki.org/?mortyurl={}'
+c.url.searchengines['l'] = 'farside.link/librex/search.php?q={}'
 c.url.searchengines['s'] = 'farside.link/searxng/search?q={}'
 c.url.searchengines['y'] = 'farside.link/invidious/search?q={}'
+c.url.searchengines['m'] = 'morty.ononoki.org/?mortyurl={}'
 c.url.searchengines['a'] = 'annas-archive.org/search?q={}'
 c.url.searchengines['DEFAULT'] = c.url.searchengines['l']
 c.url.start_pages = c.url.default_page = 'minifocs.fly.dev'
