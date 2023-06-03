6f84cd97276451e5d4a612cd9fcfad5de757f2db
Phantop <phantop@tuta.io>
2023-06-03 12:00:25 -0400

    fish: ao3 authors; bin: pad jpeg

--- fish/functions/ao3.fish
+++ fish/functions/ao3.fish
@@ -1,6 +1,8 @@
 function ao3
     grep works/.\*/b $argv | cut -d/ -f3 | uniq | parallel curl -OJL ao3.org/downloads/{}/1.epub
     for i in *.epub
-        mv $i (tar Oxf $i content.opf | pup | grep -A1 -m1 dc:title | tail -1 | sed 's/^ *//' | tr -d /).epub
+        set title (tar Oxf $i content.opf | pup | grep -A1 -m1 dc:title | tail -1 | sed 's/^ *//' | tr -d /)
+        set creator (tar Oxf $i content.opf | pup | grep -A1 -m1 dc:creator | tail -1 | sed 's/^ *//' | tr -d /)
+        mv $i "$creator - $title.epub"
     end
 end
