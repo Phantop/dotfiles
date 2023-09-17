ed63f598d4ad5487ea6f68426354fb73584fe37d
Phantop <phantop@tuta.io>
2023-09-17 16:58:45 -0400

    bin: clean up some arguments

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,11 +50,4 @@ command Q q!
 PaqSync
 
 autocmd BufReadPost,FileReadPost *.bz3 call gzip#read("bzip3 -d")
-
-let g:ale_linters = {'cpp': ['cc', 'clang', 'cppcheck']}
-let g:ale_fixers = {'cpp': ['clang-format']}
-let g:ale_linters_ignore = {'cpp': ['clangcheck', 'clangtidy']}
-let g:ale_cpp_cc_options = "-std=c++17 -Wall"
-let g:ale_cpp_clangd_options = "-std=c++17 -Wall"
-let g:ale_c_cc_options = "-Wall -lm"
 ]]
