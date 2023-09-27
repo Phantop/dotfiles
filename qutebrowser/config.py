b640b74f934408846df477981e34ecd1d5a0aaf0
Phantop <phantop@tuta.io>
2023-09-27 18:28:33 -0400

    qute: clean searches, fish: ytflux mark read

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,12 +6,11 @@ c.confirm_quit = ['downloads']
 c.downloads.location.directory = '~/Downloads'
 c.qt.workarounds.remove_service_workers = True
 
-c.url.searchengines['!'] = 'farside.link/librex/search.php?q=!{}'
-c.url.searchengines['4'] = '4get.ca/web?s={}'
+c.url.searchengines['!'] = 'farside.link/librey/search.php?q=!{}'
+c.url.searchengines['4'] = 'farside.link/4get/web?s={}'
 c.url.searchengines['a'] = 'annas-archive.org/search?q={}'
-c.url.searchengines['l'] = 'farside.link/librex/search.php?q={}'
-c.url.searchengines['m'] = 'morty.ononoki.org/?mortyurl={}'
-c.url.searchengines['n'] = 'https://search.marginalia.nu/search?query={}'
+c.url.searchengines['l'] = 'farside.link/librey/search.php?q={}'
+c.url.searchengines['m'] = 'https://search.marginalia.nu/search?query={}'
 c.url.searchengines['s'] = 'farside.link/searxng/search?q={}'
 c.url.searchengines['x'] = 'aldeid.com/wiki/X86-assembly/Instructions/{}'
 c.url.searchengines['y'] = 'farside.link/invidious/search?q={}'
