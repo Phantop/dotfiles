85e0d80e74c8cba2c853b9a19add5c227fc85d9c
Phantop <phantop@tuta.io>
2023-10-06 16:19:49 -0400

    bin: croptop

--- fish/functions/ytflux.fish
+++ fish/functions/ytflux.fish
@@ -1,5 +1,5 @@
 function ytflux
     set cat 3
-    eval curl (cat ~/.miniflux)/entries?status=unread\&category_id=$cat | jq .entries[].url | xargs yt-dlp
+    eval curl (cat ~/.miniflux)/entries?status=unread\&category_id=$cat | jq .entries[].url | xargs yt-dlp &
     eval curl -X PUT (cat ~/.miniflux)/categories/$cat/mark-all-as-read
 end
