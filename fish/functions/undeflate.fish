d99180265d5b47c5372cdeb80e97db6497531720
Phantop <phantop@tuta.io>
2023-11-27 19:04:15 -0500

    nvim: why is this config so scattereddd

--- fish/functions/undeflate.fish
+++ fish/functions/undeflate.fish
@@ -1,4 +1,4 @@
-function undefalte
+function undeflate
     fd -t f -x advzip -z0
     fd -t f -x advdef -z0
     fd -e pdf -x qpdf --stream-data=uncompress --replace-input --compress-streams=n --recompress-flate --compression-level=0 --optimize-images {}
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,33 @@
 require "paq" {
-    'ctrlpvim/ctrlp.vim';
-    'dense-analysis/ale';
-    'dracula/vim';
-    'godlygeek/tabular';
-    'lervag/vimtex';
-    'lewis6991/gitsigns.nvim';
-    'lukas-reineke/indent-blankline.nvim';
-    'luukvbaal/nnn.nvim';
-    'RobertAudi/fish.vim';
-    'tpope/vim-commentary';
-    'tpope/vim-sensible';
-    'tpope/vim-sleuth';
-    'vim-airline/vim-airline';
+    'ctrlpvim/ctrlp.vim',
+    'dense-analysis/ale',
+    'echasnovski/mini.nvim',
+    'dracula/vim',
+    'lervag/vimtex',
+    'lewis6991/gitsigns.nvim',
+    'lukas-reineke/indent-blankline.nvim',
+    'luukvbaal/nnn.nvim',
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+    'nvimtools/none-ls.nvim',
+    'tpope/vim-sleuth',
+    'vim-airline/vim-airline',
 
-    'https://git.sr.ht/~ackyshake/VimCompletesMe.vim';
-    { 'nvim-treesitter/nvim-treesitter', run = function() vim.cmd 'TSUpdate' end };
+    --'tpope/vim-sensible',
+    --'williamboman/mason-lspconfig.nvim',
+    --{ 'williamboman/mason.nvim', build = ':MasonUpdate' },
 
+    'https://sr.ht/~ackyshake/VimCompletesMe.vim',
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 }
+
 require("gitsigns").setup()
 require("ibl").setup()
+require("lspconfig").clangd.setup{}
+--require("mason").setup()
+--require("mason-lspconfig").setup()
 require("nnn").setup()
-require("nvim-treesitter.configs").setup {
-    highlight = { enable = true, additional_vim_regex_highlighting = false }
-}
+require("nvim-treesitter.configs").setup{ highlight = { enable = true } }
 
 vim.opt.background = "dark"
 vim.opt.clipboard = "unnamedplus"
@@ -44,11 +49,16 @@ vim.opt.wrap = false
 
 vim.keymap.set('', '<F7>', 'mzgg=G`z')
 vim.keymap.set('', 'r', '"_d')
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.code_action() end)
+vim.keymap.set('n', '<leader>h', function() vim.lsp.buf.hover() end)
+vim.keymap.set('n', '<leader>r', function() vim.lsp.buf.rename() end)
 
 vim.cmd [[
+PaqSync
 colorscheme dracula
 command Q q!
-PaqSync
-
+let b:ale_linters = {'c': ['']}
 autocmd BufReadPost,FileReadPost *.bz3 call gzip#read("bzip3 -d")
 ]]
