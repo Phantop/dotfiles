47a7a7708525ad353ad846450c0b595227eb41cd
Phantop <phantop@tuta.io>
2024-01-31 09:58:38 -0500

    bin: dwarfs flac support and better ytflux

--- fish/functions/ytflux.fish
+++ fish/functions/ytflux.fish
@@ -1,6 +1,6 @@
 function ytflux
-    set cat 3
-    set entries (eval curl (cat ~/.config/miniflux)/entries?status=unread\&category_id=$cat | jq -r .entries[].url)
-    eval curl -X PUT (cat ~/.config/miniflux)/categories/$cat/mark-all-as-read
-    parallel -u yt-dlp ::: $entries
+    set cat (cat ~/.config/miniflux)/categories/3
+    set entries (eval curl $cat/entries?status=unread | jq -r .entries[].url)
+    eval curl -X PUT $cat/mark-all-as-read
+    parallel -u yt-dlp $argv ::: $entries
 end
