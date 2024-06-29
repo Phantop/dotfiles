2c2d5b9ce9b37754b1c645b60ac2ad2402224a22
Phantop <phantop@tuta.io>
2024-06-28 23:08:14 -0400

    qute: clean miniflux greasemonkey + heading anchors

--- qutebrowser/greasemonkey/enclosures.js
+++ qutebrowser/greasemonkey/enclosures.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name          EnclosureExpand
-// @include       https://minifocs.fly.dev/*
+// @include       https://minifocs.fly.dev/*/entry/*
+// @include       https://minifocs.fly.dev/share/*
 // ==/UserScript==
 const details = document.querySelector("details.entry-enclosures");
 details.setAttribute("open", "");
---
+++ qutebrowser/greasemonkey/headings.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name          AnchorHeadings
+// @include       https://minifocs.fly.dev/*/entry/*
+// @include       https://minifocs.fly.dev/share/*
+// ==/UserScript==
+var tagNames = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+var tagSelector = tagNames.join();
+var $elements = document.querySelectorAll(tagSelector);
+
+for ($element of $elements) {
+  var id = $element.id;
+
+  if (!id) {
+    // Check if heading has any nested elements with id
+    var $innerElement = $element.querySelector('[id]');
+    if ($innerElement) { id = $innerElement.id; }
+    else { continue; }
+  }
+
+  // Create anchor if not already present
+  if (!$element.querySelector('[href]'))
+    $element.appendChild(createAnchor(id));
+}
+
+function createAnchor(id) {
+  var $link = document.createElement('a');
+  $link.href = '#' + id;
+  $link.appendChild(document.createTextNode('♯'));
+  return $link;
+}
