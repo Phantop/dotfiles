bb84fb6d057de634464eee5e2c05de0bc97da420
Phantop <phantop@tuta.io>
2025-01-03 17:00:31 -0500

    fish: more arista stuff, qute: more adblock lists

--- qutebrowser/adblock.py
+++ qutebrowser/adblock.py
@@ -3,16 +3,19 @@ host("https://www.github.developerdan.com/hosts/lists/facebook-extended.txt")
 
 c.content.blocking.adblock.lists = []
 abp = c.content.blocking.adblock.lists.append
-abp("https://fanboy.co.nz/r/fanboy-ultimate.txt")
-abp("https://fanboy.co.nz/fanboy-antifacebook.txt")
-abp("https://fanboy.co.nz/fanboy-annoyance.txt")
-abp("https://fanboy.co.nz/fanboy-cookiemonster.txt")
-abp("https://easylist-downloads.adblockplus.org/antiadblockfilters.txt")
 abp("https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt")
-abp("https://easylist-downloads.adblockplus.org/easylist.txt")
-abp("https://easylist-downloads.adblockplus.org/easyprivacy.txt")
+abp("https://easylist-downloads.adblockplus.org/antiadblockfilters.txt")
+abp("https://fanboy.co.nz/fanboy-antifacebook.txt")
+abp("https://fanboy.co.nz/fanboy-cookiemonster.txt")
+abp("https://fanboy.co.nz/r/fanboy-ultimate.txt")
 
-abp("https://github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt")
 abp("https://github.com/DandelionSprout/adfilt/raw/master/AnnoyancesList")
-abp("https://github.com/DandelionSprout/adfilt/raw/master/SocialShareList.txt")
 abp("https://github.com/DandelionSprout/adfilt/raw/master/ExtremelyCondensedList.txt")
+abp("https://github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt")
+abp("https://github.com/DandelionSprout/adfilt/raw/master/SocialShareList.txt")
+
+abp("https://github.com/hagezi/dns-blocklists/raw/main/adblock/pro.mini.txt")
+abp("https://github.com/iam-py-test/uBlock-combo/raw/main/list.txt")
+abp("https://github.com/liamengland1/miscfilters/raw/master/antipaywall.txt")
+abp("https://github.com/mchangrh/yt-neuter/raw/main/yt-neuter.txt")
+abp("https://github.com/yokoffing/filterlists/raw/main/privacy_essentials.txt")
