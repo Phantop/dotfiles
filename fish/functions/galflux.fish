ab84fa14c2f1bc7e03ffa3a2fb630621760bc60e
Phantop <phantop@tuta.io>
2025-01-17 11:58:12 -0500

    fish: account for priviblur and more work functions

--- fish/functions/galflux.fish
+++ fish/functions/galflux.fish
@@ -2,5 +2,5 @@ function galflux
     set cat (cat ~/.config/miniflux)/categories/2
     set entries (eval curl $cat/entries?status=unread | jq -r .entries[].url)
     eval curl -X PUT $cat/mark-all-as-read
-    parallel -u gallery-dl ::: $entries
+    parallel -u 'gallery-dl (trurl -s host=www.tumblr.com {})' ::: $entries
 end
