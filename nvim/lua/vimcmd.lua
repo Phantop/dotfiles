80840b6bb4e126917e40075b3bd1d3afcea7d7f8
Phantop <phantop@tuta.io>
2025-01-17 11:48:26 -0500

    nvim: fix airline

--- nvim/lua/vimcmd.lua
+++ nvim/lua/vimcmd.lua
@@ -4,4 +4,5 @@ colorscheme dracula
 command Q q!
 let b:ale_linters = {'c': [''], 'sh': [''], 'php': ['']}
 let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
+let g:airline#extensions#whitespace#symbol = '!'
 ]]
