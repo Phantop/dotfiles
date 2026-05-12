15fcdf2bd0d1965254fa7000d2096dab613a1d24
Julie Hakimi <jhakimi@arista.com>
2026-05-11 22:17:42 -0400

    fish: remove unused miniflux functions

--- fish/functions/reflux.fish
+++
@@ -1,5 +0,0 @@
-function reflux
-    for i in (curlflux "$argv"feeds | jq -rc '.[] | select(.parsing_error_count!=0)')
-        eval curl "$(cat ~/.config/miniflux)/feeds/$(echo $i | jq -r .id)/refresh" -X PUT
-    end
-end
